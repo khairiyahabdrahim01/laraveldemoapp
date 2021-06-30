ba052e2932d9c5eba49eb9d572133bcc76dfa0a2
khairiyahabdrahim01 <79837967+khairiyahabdrahim01@users.noreply.github.com>
2021-06-30 15:58:39 +0800

    Initial commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,43 @@
+@extends('app')
+
+@section('content')
+
+<main class="login-form">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <h3 class="card-header text-center">Login</h3>
+                    <div class="card-body">
+                        <form action="{{ route('login.custom') }}" method="post">
+                            @csrf 
+                            <div class="form-group mb-3">
+                                <input type="text" name="email" id="email" placeholder="Email" class="form-control" required autofocus>
+                                @if ($errors->has('email'))
+                                <span class="text-danger">{{ $errors->first('email') }}</span>
+                                @endif
+                            </div>
+                            <div class="form-group mb-3">
+                                <input type="password" name="password" id="password" placeholder="Password" class="form-control" required>
+                                @if ($errors->has('password'))
+                                <span class="text-danger">{{ $errors->first('password') }}</span>
+                                @endif
+                            </div>
+                            <div class="form-group mb-3">
+                                <div class="checkbox">
+                                    <label for="remember">
+                                        <input type="checkbox" name="remember" id="remember"> Remember Me
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="d-grid mx-auto">
+                                <button type="submit" class="btn btn-dark btn-block">Sign-In</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+@endsection
---
+++ resources/views/auth/registration.blade.php
@@ -0,0 +1,48 @@
+@extends('app')
+
+@section('content')
+<main class="signup-form">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <h3 class="card-header text-center">Register User</h3>
+                    <div class="card-body">
+                        <form action="{{ route('register.custom') }}" method="post">
+                            @csrf 
+                            <div class="form-group mb-3">
+                                <input type="text" placeholder="Name" name="name" id="name" class="form-control" required autofocus>
+                                @if ($errors->has('name'))
+                                <span class="text-danger">{{ $errors->first('name') }}</span>
+                                @endif
+                            </div>
+                            <div class="form-group mb-3">
+                                <input type="text" placeholder="Email" name="email" id="email" class="form-control" required autofocus>
+                                @if ($errors->has('email'))
+                                <span class="text-danger">{{ $errors->first('email') }}</span>
+                                @endif
+                            </div>
+                            <div class="form-group mb-3">
+                                <input type="password" placeholder="Password" name="password" id="password" class="form-control" required>
+                                @if ($errors->has('password'))
+                                <span class="text-danger">{{ $errors->first('password') }}</span>
+                                @endif
+                            </div>
+                            <div class="form-group mb-3">
+                                <div class="checkbox">
+                                    <label for="checkbox">
+                                        <input type="checkbox" name="remember" id="remember"> Remember Me
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="d-grid mx-auto">
+                                <button type="submit" class="btn btn-dark btn-block">Sign up</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+@endsection
